12b58cae986ee1eed757f23decf92d603d6b67b2
ahielg <ahielg@liveperson.com>
2020-12-20 16:52:51 +0200

    add bank producer

---
+++ src/main/java/com/ahiel/bank/dto/BankProducer.java
@@ -0,0 +1,96 @@
+package com.ahiel.bank.dto;
+
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.ser.std.StringSerializer;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.time.Instant;
+import java.util.Properties;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * @author Ahielg
+ * @date 20/12/2020
+ */
+@SuppressWarnings("ObjectAllocationInLoop")
+@Slf4j
+public class BankProducer {
+
+    public static void main(String[] args) {
+        new BankProducer().run();
+    }
+
+    public static ProducerRecord<String, String> newRandomTransaction(String name) {
+        // creates an empty json {}
+        ObjectNode transaction = JsonNodeFactory.instance.objectNode();
+
+        // { "amount" : 46 } (46 is a random number between 0 and 100 excluded)
+        Integer amount = ThreadLocalRandom.current().nextInt(0, 100);
+
+        // Instant.now() is to get the current time using Java 8
+        Instant now = Instant.now();
+
+        // we write the data to the json document
+        transaction.put("name", name);
+        transaction.put("amount", amount);
+        transaction.put("time", now.toString());
+        return new ProducerRecord<>("bank-transactions", name, transaction.toString());
+    }
+
+    public KafkaProducer<String, String> createKafkaProducer() {
+        String bootstrapServers = "127.0.0.1:9092";
+
+        // create Producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // create safe Producer
+        properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+        properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
+        properties.setProperty(ProducerConfig.RETRIES_CONFIG, "5");
+        properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5"); // kafka 2.0 >= 1.1 so we can keep this as 5. Use 1 otherwise.
+
+        // high throughput producer (at the expense of a bit of latency and CPU usage)
+        //properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "1");
+        //properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(32 * 1024)); // 32 KB batch size
+
+        // create the producer
+        return new KafkaProducer<>(properties);
+    }
+
+    @SuppressWarnings("BusyWait")
+    public void run() {
+
+        log.info("Setup");
+
+        // create a kafka producer
+        KafkaProducer<String, String> producer = createKafkaProducer();
+
+
+        int i = 0;
+        while (true) {
+            log.info("Producing batch: {}", i);
+            try {
+                producer.send(newRandomTransaction("john"));
+                Thread.sleep(100);
+                producer.send(newRandomTransaction("stephane"));
+                Thread.sleep(100);
+                producer.send(newRandomTransaction("alice"));
+                Thread.sleep(100);
+                i += 1;
+            } catch (InterruptedException e) {
+                break;
+            }
+        }
+
+        log.info("End of application");
+    }
+
+}
